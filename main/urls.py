28fd8ccda7233a5345bd68f046ff63ece0868842
SuperMonkey0410 <145975354+SuperMonkey0410@users.noreply.github.com>
2025-02-11 19:31:43 +0300

    добавление категорий, редакция base,index.html

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),  # URL для страницы с деталями товара
-    path('about/', views.about, name='about'),
+    path('catalog/<slug:category_slug>', views.catalog, name='product_catalog'),
+    path('product/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    # URL для страницы с деталями товара
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Product, Category
@@ -9,14 +9,22 @@ from .models import Product, Category
 def index(request):
     """Вывод главной страницы"""
     products = Product.objects.all()
-    context = {'products': products, }
+    categories = Category.objects.all
+    search = request.GET.get('q', None)
+    context = {'products': products, 'categories': categories}
     return render(request, 'main/index.html', context=context)
 
 
-def about(request):
-    return HttpResponse('Это страница о нас')
-
-def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
-    context = {'product': product}
+def product_detail(request, product_slug):
+    product = Product.objects.get(slug=product_slug)
+    categories = Category.objects.all()
+    context = {'product': product, 'categories': categories}
     return render(request, 'main/product_detail.html', context=context)
+
+
+def catalog(request, category_slug):
+    products = Product.objects.filter(category__slug=category_slug)
+    categories = Category.objects.all()
+    category = Category.objects.get(slug=category_slug)
+    context = {'products': products, 'categories': categories, 'category': category}
+    return render(request, 'main/catalog.html', context=context)
