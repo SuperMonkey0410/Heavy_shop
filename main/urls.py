42ec5f6daa6c4756342865ddba571f64291a0e29
SuperMonkey0410 <145975354+SuperMonkey0410@users.noreply.github.com>
2025-02-15 12:46:50 +0300

    добавил search_query

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.index, name='search'),
     path('catalog/<slug:category_slug>', views.index, name='product_catalog'),
     path('product/<slug:product_slug>/', views.product_detail, name='product_detail'),
     # URL для страницы с деталями товара
--- main/views.py
+++ main/views.py
@@ -1,21 +1,30 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import Paginator
-
 from .models import Product, Category
+from .utils import q_search
 
-
-# Create your views here.
 
 def index(request, category_slug=None):
     """Вывод главной страницы + фильтрация по категориям"""
     category_list = Category.objects.all()
+    search_query = request.GET.get('q', None)
+
+    # Инициализация переменной categories
+    categories = Category.objects.all()
+
     if category_slug:
         categories = Category.objects.filter(slug=category_slug)
         products_list = Product.objects.filter(category__slug=category_slug)
+
+    elif search_query:
+        products_list = Product.objects.filter(
+            Q(title__icontains=search_query) | Q(description__icontains=search_query) | Q(brand__title__icontains=search_query)
+        )
+
     else:
         products_list = Product.objects.all()
-        categories = Category.objects.all()
 
     paginator = Paginator(products_list, 6)
     page_number = request.GET.get('page')
@@ -23,11 +32,10 @@ def index(request, category_slug=None):
     context = {'products': products, 'categories': categories, 'category_list': category_list}
     return render(request, 'main/index.html', context=context)
 
-
 def product_detail(request, product_slug):
     product = Product.objects.get(slug=product_slug)
-    categories = Category.objects.all()
-    context = {'product': product, 'categories': categories}
+    category_list = Category.objects.all()
+    context = {'product': product, 'category_list': category_list}
     return render(request, 'main/product_detail.html', context=context)
 
 
